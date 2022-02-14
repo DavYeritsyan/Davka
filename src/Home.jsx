898113ae253a2d0f9d1c3bb9f2aff900ed189c56
DavYeritsyan <davityeritsyan96@gmail.com>
2022-02-14 11:06:28 +0400

    add state

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react"
+
 export  let Home=()=>{
+    let [state,setstate]=useState()
     return(
         <div>
             home is working
             <div>JBHUHUU</div>
             <div>JBHUHUU</div>
+            <div>es nor bana</div>
+            <div>verjnakan</div>
         </div>
     )
 }
